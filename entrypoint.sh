b869cb29b5a84d3d8ce8a9af51f1c207207eb444
Tim Dumol <tim@timdumol.com>
2017-11-21 13:51:37 +0800

    Allow using user IDs pre-existing inside the image.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 USER_ID=${LOCAL_USER_ID:-9001}
 
-adduser -s /bin/bash -u $USER_ID -S -D -h /dehydrated user
-chown -R user /dehydrated
+if ! getent passwd $USER_ID; then
+    adduser -s /bin/bash -u $USER_ID -S -D -h /dehydrated user
+fi
+chown -R $USER_ID /dehydrated
 export HOME=/dehydrated
 
-exec /usr/local/bin/gosu user dockerize -template config.tmpl:config /bin/bash dehydrated -t dns-01 -k hooks/cloudflare/hook.py $@
+exec /usr/local/bin/gosu "$USER_ID" dockerize -template config.tmpl:config /bin/bash dehydrated -t dns-01 -k hooks/cloudflare/hook.py $@
